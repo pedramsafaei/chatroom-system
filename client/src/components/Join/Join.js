5503f86777de0dcf6b83d80231d631b0a2e2ed0a
Pedram Safaei <psafaei@nevada.unr.edu>
2020-04-19 08:33:04 -0700

    made a login screen and added style

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -2,39 +2,43 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./Join.css";
+import "./login";
 
 const Join = () => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
 
   return (
-    <div className="joinOuterContainer">
-      <div className="joinInnerContainer">
-        <h1 className="heading">Join</h1>
-        <div>
+    <div className="mainBody">
+      <div class="login">
+        <header class="header">
+          <span class="text">Login here</span>
+          <span class="loader"></span>
+        </header>
+        <form class="form">
           <input
             placeholder="Name"
             className="joinInput"
             type="text"
             onChange={(event) => setName(event.target.value)}
           ></input>
-        </div>
-        <div>
+
           <input
             placeholder="Room"
-            className="joinInput mt-20"
+            className="joinInput"
             type="text"
             onChange={(event) => setRoom(event.target.value)}
           ></input>
-        </div>
-        <Link
-          onClick={(event) => (!name || !room ? event.preventDefault() : null)}
-          to={`/chat?name=${name}&room=${room}`}
-        >
-          <button className="button mt-20" type="submit">
-            Sign In
-          </button>
-        </Link>
+
+          <Link
+            onClick={(event) =>
+              !name || !room ? event.preventDefault() : null
+            }
+            to={`/chat?name=${name}&room=${room}`}
+          >
+            <button className="btn" type="submit"></button>
+          </Link>
+        </form>
       </div>
     </div>
   );
